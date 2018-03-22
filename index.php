d89b48c6cded5c7c677f2b1c08f1e302d21ba724
Esteban Martinez <fullstackmartinez@gmail.com>
2018-03-22 12:22:47 -0600

    added jquery script tag in head

--- index.php
+++ index.php
@@ -3,9 +3,16 @@
 	<head>
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet"
 				integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 		<title>How far is your trip?</title>
+		<style>
+			#map {
+				height: 300px;
+				width: 75%;
+			}
+		</style>
 	</head>
 	<body>
 		<header>
