d989385438644ce3b2fa4f66ee292be6064b098c
Esteban Martinez <fullstackmartinez@gmail.com>
2018-03-22 11:31:21 -0600

    h1 and p tag

--- index.php
+++ index.php
@@ -5,8 +5,13 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet"
 				integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+		<title>How far is your trip?</title>
 	</head>
 	<body>
+		<header>
+			<h1>How Far Is Your Trip?</h1>
+			<p>This page is intended to take two points and calculate the distance and travel duration between them using Google Maps.</p>
+		</header>
 
 	</body>
 </html>
